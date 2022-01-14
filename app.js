7e7120692a0241bb6e451d2e57fdc5a88ed7c501
Raunak Shrestha <raunakshrestha@Vulcan.local>
2022-01-14 14:49:42 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const getPlayerChoice = function(){
+  const selection = prompt("Rock, paper or Scissors?", "n ");
+}
+startGameBtn.addEventListener("click",function(){
+  console.log("Game is starting...");
+});
